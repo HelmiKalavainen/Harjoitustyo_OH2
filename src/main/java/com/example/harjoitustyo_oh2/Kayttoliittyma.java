8dca2252b06e697b4c6f8c0a7b2465f8b1fea41f
HelmiKalavainen <147623200+HelmiKalavainen@users.noreply.github.com>
2024-04-12 17:50:34 +0300

    initial commit

--- src/main/java/com/example/harjoitustyo_oh2/Kayttoliittyma.java
+++ src/main/java/com/example/harjoitustyo_oh2/Kayttoliittyma.java
@@ -1,5 +1,4 @@
 package com.example.harjoitustyo_oh2;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,38 +8,72 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
-
 import java.io.*;
 import java.util.StringTokenizer;
 
+/**
+ * Luokka Kayttoliittyma toteuttaa yksinkertaisen tuotteiden tallentimen
+ *
+ */
 public class Kayttoliittyma extends Application {
-    //textfieldit joihin syötetään tuote sen tunnus ja hinta
+    /**
+     * Tuotteen nimi teksitkenttä.
+     */
     private TextField tfTuote = new TextField();
+    /**
+     * Tuotteen tunnus tekstikenttä.
+     */
     private TextField tfTunnus = new TextField();
+    /**
+     * Tuotteen hinta tekstikenttä.
+     */
     private TextField tfHinta = new TextField();
+    /**
+     * Kappalemäärä tekstikenttä.
+     */
     private TextField tfMaara = new TextField();
-    //tallennus- ja haku napit
+    /**
+     * Tallenna tuote painike.
+     */
     private Button btTallenna = new Button("Tallenna");
+    /**
+     * Hae tuotetta painike.
+     */
 
     private Button btHae = new Button("Hae tuotetta");
 
-
+    /**
+     * @param args
+     * Testipääohjelma, ei parametreja
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * @param primaryStage
+     * Ohjelmaikkunan kaynnistyksen ja toiminnallisuuden maarittely
+     */
     @Override
     public void start(Stage primaryStage) {
-
+        //paneeli
         GridPane paneeli = new GridPane();
         final VBox vbox = new VBox();
+        //asetetaan tilaa solujen välille
         vbox.setPadding(new Insets(10, 10, 10, 10));
         paneeli.setHgap(10);
         paneeli.setVgap(10);
-
+        //asetetaan tekstikentät koordinaatein paneeliin
+        /**
+         * tekstialue tuote tekstikentälle
+         */
         paneeli.add(new Label("Tuote:"), 0, 0);
         paneeli.add(tfTuote, 1, 0);
+        /**
+         * tekstialue tunnus tekstikentälle
+         */
 
         paneeli.add(new Label("Tunnus:"), 0, 1);
         paneeli.add(tfTunnus, 1, 1);
@@ -55,7 +88,8 @@ public class Kayttoliittyma extends Application {
 
         //table view hakutulokselle
         TableView tvTulos = new TableView<Tuote>();
-        //taulukon sarakkeet
+
+        //taulukon sarakkeet ja niille asetetut datatyypit
         TableColumn tcTuote = new TableColumn<Tuote, String>("Tuote");
         TableColumn tcTunnus = new TableColumn<Tuote, Integer>("Tunnus");
         TableColumn tcHinta = new TableColumn<Tuote, Double>("Hinta");
@@ -68,9 +102,13 @@ public class Kayttoliittyma extends Application {
         tvTulos.setItems(tuoteLista);
         tvTulos.getColumns().addAll(tcTuote, tcTunnus, tcHinta, tcMaara);
 
+/**
+ * Tallentaa tuotteiden tiedot tiedostoon tuotteet.txt.
+ *
+ */
         btTallenna.setOnAction(ActionEvent -> {
 
-            // Oletus värit
+            // Oletus värit, css tiedoston stylesheet:stä
             tfTuote.setStyle("-fx-border-color: blue; -fx-border-width: 1px ;");
             tfTunnus.setStyle("-fx-border-color: blue; -fx-border-width: 1px ;");
             tfHinta.setStyle("-fx-border-color: blue; -fx-border-width: 1px ;");
@@ -84,7 +122,7 @@ public class Kayttoliittyma extends Application {
             }
 
 
-            // Tarkistetaan onko arvo numero, opetusavustaja auttoi.
+            // Tarkistetaan onko arvo numero, opetusavustaja auttoi tässä.
             // Lähde: https://stackoverflow.com/questions/5238491/check-if-string-contains-only-letters
             if (!isNumeric(tfTunnus.getText())) {
                 tfTunnus.setStyle("-fx-border-color: red; -fx-border-width: 2px ;");
@@ -96,7 +134,7 @@ public class Kayttoliittyma extends Application {
                 error = true;
             }
 
-            if (!isNumeric(tfHinta.getText())) {
+            if (!isNumeric(tfMaara.getText())) {
                 tfMaara.setStyle("-fx-border-color: red; -fx-border-width: 2px ;");
                 error = true;
             }
@@ -120,6 +158,9 @@ public class Kayttoliittyma extends Application {
             }
         });
 
+
+        //Huom! jos tiedestossa on tyhjä rivi, se aiheuttaa virheen tuotetta hakiessa
+
         btHae.setOnAction(e -> {
             int tunnus = Integer.parseInt(tfTunnus.getText());
             tuoteLista.clear();
@@ -135,9 +176,21 @@ public class Kayttoliittyma extends Application {
                     if (tunnus == tunnusnro) tuoteLista.add(new Tuote(tunnus, nimi, hinta, maara));
                 }
             } catch (IOException ex) {
+                System.out.println("Tiedostoa ei löytynyt!");
                 throw new RuntimeException(ex);
             }
-
+            //popup ikkuna jos haetulla tuotenumerolla ei löydy tuotteita
+                Popup virhe = new Popup();
+                Label lbVirhe = new Label("Virhe! Tuotteita ei löytynyt tai tuotenumero on väärä.");
+                lbVirhe.setStyle("-fx-background-color: white; ");
+                lbVirhe.setStyle("-fx-border-color: red; -fx-border-width: 1px ;");
+                virhe.getContent().add(lbVirhe);
+                lbVirhe.setMinWidth(100);
+                lbVirhe.setMinHeight(80);
+                virhe.setAutoHide(true);
+            if (tuoteLista.isEmpty()) {
+                virhe.show(primaryStage);
+            }
         });
 
 
@@ -156,15 +209,16 @@ public class Kayttoliittyma extends Application {
         primaryStage.show();
     }
 
-
         /**
          * @author https://stackoverflow.com/questions/1102891/how-to-check-if-a-string-is-numeric-in-java
+         *
          * */
         public static boolean isNumeric (String str){
             try {
                 Double.parseDouble(str);
                 return true;
             } catch (NumberFormatException e) {
+                System.out.println("Virhe!");
                 return false;
             }
         }
--- src/main/java/com/example/harjoitustyo_oh2/Tuote.java
+++ src/main/java/com/example/harjoitustyo_oh2/Tuote.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Scanner;
 
 /**
- * Luokka toteuttaa Tuotteen jolla on tuotenumero, hinta ja nimi
+ * Luokka toteuttaa Tuotteen jolla on tuotenumero, hinta, nimi ja määrä.
  */
 
 public class Tuote implements Serializable {
@@ -27,9 +27,19 @@ public class Tuote implements Serializable {
 
     private String nimi;
 
+    /**
+     * kappalemäärä kokonaislukuna.
+     */
+
     private int maara;
 
-    //tuote- luokka
+    /**
+     * Tuote alustaja
+     * @param tuotenro tuote kohtainen ID numero
+     * @param nimi tuotteen nimi
+     * @param hinta tuotteen kappale hinta
+     * @param maara tuotteiden määrä
+     */
     public Tuote(int tuotenro, String nimi, double hinta, int maara) {
         this.tuotenro = tuotenro;
         this.hinta = hinta;
@@ -37,13 +47,17 @@ public class Tuote implements Serializable {
         this.maara = maara;
     }
 
+    /**
+     * Testipääohjelma, ei parametreja
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
         System.out.print("Tuotenumero: ");
         int tuotenro = lukija.nextInt();
 
-        lukija.nextLine(); // rivinvaihdon, joka jäi nextInt():n jälkeen(???)
+        lukija.nextLine();
 
         System.out.print("Tuotteen nimi: ");
         String nimi = lukija.nextLine();
@@ -57,34 +71,66 @@ public class Tuote implements Serializable {
 
     }
 
+    /**
+     * palauttaa nimen
+     * @return String
+     */
     public String getNimi() {
         return nimi;
     }
 
+    /**
+     * @param nimi
+     * asettaa nimen
+     */
     public void setNimi(String nimi) {
         this.nimi = nimi;
     }
 
+    /**
+     * @return
+     * palauttaa hinnan
+     */
     public double getHinta() {
         return hinta;
     }
 
+    /**
+     * @param hinta
+     *  asettaa hinnan
+     */
     public void setHinta(double hinta) {
         this.hinta = hinta;
     }
 
+    /**
+     * @return
+     *  palauttaa tuotenumeron
+     */
     public int getTuotenro() {
         return tuotenro;
     }
 
+    /**
+     * @param tuotenro
+     * asettaa tuotenumeron
+     */
     public void setTuotenro(int tuotenro) {
         this.tuotenro = tuotenro;
     }
 
+    /**
+     * @return
+     * palauttaa määrän
+     */
     public int getMaara() {
         return maara;
     }
 
+    /**
+     * @param maara
+     *  asettaa määrän
+     */
     public void setMaara(int maara) {
         this.maara = maara;
     }
